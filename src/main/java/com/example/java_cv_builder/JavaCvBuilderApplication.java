03a225a641cbdecc5d9b4b9939b8dcf35cc1cb94
chebelok <lotokhinivan@gmail.com>
2023-06-06 00:21:04 +0300

    initial commit

---
+++ src/main/java/com/example/java_cv_builder/JavaCvBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_cv_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCvBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCvBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_cv_builder/JavaCvBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_cv_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCvBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
