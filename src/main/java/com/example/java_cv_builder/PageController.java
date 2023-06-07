fa9a6cae47681f199b87e0444a03b7d9ebc4ef79
chebelok <lotokhinivan@gmail.com>
2023-06-07 18:12:44 +0300

    oops, forgot to include pagecontroller in previous commit)))

---
+++ src/main/java/com/example/java_cv_builder/PageController.java
@@ -0,0 +1,19 @@
+package com.example.java_cv_builder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class PageController {
+
+}
